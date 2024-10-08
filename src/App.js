a6a67e6c635f3f6be57258cdcd6fde36ddc6cddc
RoshanKhalinG <roshanraee10github@gmail.com>
2024-10-08 21:35:34 +0545

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { ThemeProvider } from '@emotion/react';
 import './App.css';
+import styled from '@emotion/styled';
+import { DarkMode } from '@mui/icons-material';
+import { darkTheme } from './utils/Themes';
+import Hero from './componants/HeroSection/Index';
 
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      
+      <Navbar/>
+           <Body>
+              <Hero></Hero>
+           </Body>
+    </ThemeProvider>
   );
 }
-
+ 
 export default App;
