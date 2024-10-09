cbc80a63186bdf63a0c9a1094e539b8a3bcd90e2
RoshanKhalinG <roshanraee10github@gmail.com>
2024-10-09 19:48:26 +0545

    second

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import { ThemeProvider } from '@emotion/react';
-import './App.css';
-import styled from '@emotion/styled';
-import { DarkMode } from '@mui/icons-material';
-import { darkTheme } from './utils/Themes';
-import Hero from './componants/HeroSection/Index';
+import Navbar from "./components/Navbar"; // Ensure Navbar.js exists
+import HeroSection from "./components/HeroSection"; // Ensure HeroSection.js exists
+import About from "./components/About"; // Ensure About.js exists
+import Skills from "./components/Skills"; // Ensure Skills.js exists
+import Experience from "./components/Experience"; // Ensure Experience.js exists
+import Education from "./components/Education"; // Ensure Education.js exists
+import ProjectDetails from "./components/ProjectDetails"; // Ensure ProjectDetails.js exists
+import Projects from "./components/Projects"; // Ensure Projects.js exists
+import Contact from "./components/Contact"; // Ensure Contact.js exists
+import Footer from "./components/Footer"; // Ensure Footer.js exists
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
   width: 100%;
-  height: 100%;
   overflow-x: hidden;
 `
+
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  const [openModal, setOpenModal] = useState({ state: false, project: null });
+  console.log(openModal)
   return (
-    <ThemeProvider theme={darkTheme}>
-      
-      <Navbar/>
-           <Body>
-              <Hero></Hero>
-           </Body>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Router >
+        <Navbar />
+        <Body>
+          <HeroSection />
+          <Wrapper>
+            <Skills />
+            <Experience />
+          </Wrapper>
+          <Projects openModal={openModal} setOpenModal={setOpenModal} />
+          <Wrapper>
+            <Education />
+            <Contact />
+          </Wrapper>
+          <Footer />
+          {openModal.state &&
+            <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
+          }
+        </Body>
+      </Router>
     </ThemeProvider>
   );
 }
- 
+
 export default App;
